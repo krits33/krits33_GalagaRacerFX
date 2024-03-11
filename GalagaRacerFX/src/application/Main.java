4dc86404290c349eae0e20d2b4e359c8881a5290
krits33 <kritikabissessur03@gmail.com>
2024-03-12 01:42:00 +0400

    spaceship and rocks

---
+++ GalagaRacerFX/src/application/Main.java
@@ -0,0 +1,149 @@
+
+package application;
+import java.io.File;
+
+import application.UI.HUD;
+import application.UI.Menus;
+import application.UI.PauseScreen;
+import javafx.application.Application;
+import javafx.geometry.Point3D;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.SubScene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.Light;
+import javafx.scene.effect.Lighting;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Shape3D;
+import javafx.scene.shape.Sphere;
+import javafx.scene.text.Font;
+
+
+public class Main extends Application {
+	// General convention upheld without code; the FIRST HALF of the boxes are always the right ones.s
+	// Constants
+	public static final int WIDTH 			= 1400;
+	public static final int HEIGHT 			= 800;
+	
+	public static GameState gameState = GameState.RUNNING;
+
+
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Lane lane = new Lane(30, 90, new Point3D(20, 100, 100));
+
+			Group group = new Group();
+			
+			
+			LightHandler.setAmbientLight(group, Color.BLACK);
+			
+			lane.addLaneToGroup(group);
+			
+			//gameCamera
+			
+			GameCamera c = new GameCamera();
+			c.setCamera(0, 0, -10);
+			c.setNearFarClip(1, 4000);
+			
+			
+			// player
+			PlayerShip player = new PlayerShip(c, new Point3D(0,0,140), new Sphere(10));
+			
+			// GUI
+			
+			HUD hud = new HUD(new Point3D(((-Main.WIDTH * 0.5) + 100), (- Main.HEIGHT * 0.5) + 65, 1250));
+			
+			PauseScreen pause = new PauseScreen(group, player, lane.getPillarsList());
+			
+			hud = Menus.createHUD(hud);
+			pause.screen = Menus.createPauseScreen(pause);
+			
+			group.getChildren().addAll(pause.screen, hud.screen);
+	        
+			
+			// Scene
+		
+			Scene scene = new Scene(group, WIDTH, HEIGHT, true);
+			scene.setFill(Color.BLACK);
+			scene.setCamera(c.camera);
+	        
+	        pause.screen.setLayoutX(scene.getWidth() - pause.screen.getWidth() / 2);
+	        pause.screen.setLayoutY(scene.getHeight() - pause.screen.getHeight() / 2);
+
+			
+			PhongMaterial asteroidMat = new PhongMaterial();
+
+			asteroidMat.setBumpMap(new Image(String.valueOf(new File("file:./src/application/Assets/asteroidBump.png"))));
+			asteroidMat.setDiffuseMap(new Image(String.valueOf(new File("file:./src/application/Assets/asteroidDiff.png"))));
+			
+			// obstacles
+			StaticEntity obstacle = new StaticEntity(
+					asteroidMat,							//material
+					10, 									//radius
+					30, 									//numOfEntities
+					player, 								//playerReference
+					new Point3D(200, 150, 100000),			//coordinateSpread
+					new Point3D(0,0,0)						//velocitySpread
+					);
+			
+			// star particle effect
+			StaticEntity star_particles = new StaticEntity(
+					new PhongMaterial(Color.WHITE),			//material
+					0.4, 									//radius
+					700, 									//numOfEntities
+					player, 								//playerReference
+					new Point3D(500, 500, 100000),			//coordinateSpread
+					new Point3D(0,0,0)						//velocitySpread
+					);
+
+			group.getChildren().add(obstacle.getEntityGroup());
+			group.getChildren().add(star_particles.getEntityGroup());
+			
+			player.setCameraOffset(new Point3D(0, -20, -150));
+			
+			group.getChildren().add(player.getShipModel());
+			
+
+			ControlShip controller = new ControlShip(player, group, hud, pause, scene, 10, 50.0, 0.05);
+			
+			LightHandler.addLightInstance(group, Color.WHITE, new Point3D(0,0,-100));
+			
+			
+			LightInstance headlight = LightHandler.addLightInstance(group, Color.WHITE,  player.getCurrPosition() );
+			LightHandler.bindLightToObject(headlight, player.getShipModel(), new Point3D(0,0,5000));
+
+			group.getChildren().add(controller.particleGroup);
+			
+			// UI Offset
+			Point3D UI_Offset = new Point3D((-WIDTH * 1.5) + 605, (-HEIGHT * 1.5) + 280, 500);
+
+//			LightHandler.getAllLightSources(primaryStage);
+			controller.startGameLoop(lane, UI_Offset, c, obstacle, star_particles);
+			
+			primaryStage.setTitle("GalaRacerFx");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+	
---
+++ GalagaRacerFX/src/application/UI/PauseScreen.java
@@ -0,0 +1,88 @@
+package application.UI;
+
+import java.util.ArrayList;
+
+import application.GameState;
+import application.Main;
+import application.PlayerShip;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Box;
+
+public class PauseScreen {
+	
+	public BorderPane screen;	
+	public Boolean isDisplayed = false;
+	
+	// references
+	private PlayerShip player;
+	private Group gameGroup;
+	private ArrayList<Box> pillars;
+	
+	
+	public PauseScreen(Group gameGroup, PlayerShip player, ArrayList<Box> pillars) {
+		this.screen = new BorderPane();
+		this.isDisplayed = false;
+		
+		this.gameGroup = gameGroup;
+		this.player = player;
+		this.pillars = pillars;
+		
+	}
+
+	public void togglePause() {
+	    if (Main.gameState == GameState.RUNNING) {
+	    	Main.gameState = GameState.PAUSED;
+	        isDisplayed = true;
+	        screen.setVisible(isDisplayed); // Show pause screen
+	        toggleNodesVisibility(gameGroup);
+	        System.out.println("Pause Screen Bounds: " + screen.getBoundsInParent());
+	    } else if (Main.gameState == GameState.PAUSED) {
+	    	Main.gameState = GameState.RUNNING;
+	        isDisplayed = false;
+	        screen.setVisible(isDisplayed); // Hide pause screen
+	        toggleNodesVisibility(gameGroup);
+	    }
+	    
+	}
+
+    public void toggleVisibility() {
+        isDisplayed = !isDisplayed;
+        updateScreenVisibility();
+    }
+	
+    private void updateScreenVisibility() {
+        screen.setVisible(isDisplayed);
+    }
+    
+    private void toggleNodesVisibility(Group gameGroup) {	
+        double pauseScreenZ = screen.getTranslateZ() - 200;
+
+        for (int i = 0; i < gameGroup.getChildren().size(); i++) {
+        	Node currentNode = gameGroup.getChildren().get(i);
+        	
+        	// we dont want to display the player when the pause screen is shown
+        	
+        	if ( (pillars.contains(currentNode) ||currentNode == player.getShipModel()) && currentNode != screen )
+        	{
+        		  double nodeZ = currentNode.getTranslateZ();
+	              if (isDisplayed)
+	              {
+	                  if (nodeZ > pauseScreenZ) {
+	                	  currentNode.setVisible(false);
+	                  } else {
+	                	  currentNode.setVisible(true);
+	                  }
+	              }
+	              else
+	              {
+	            	  currentNode.setVisible(true);
+	              }
+	        	}
+        }
+        
+    }
+}
+
+
